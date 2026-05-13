65ac4fff682d5b8450672bb24d3ee39027b44582
William Chen <william_w_chen@trendmicro.com>
2026-05-13 23:54:06 +0800

    refactor: remove unused upgrade suggestion logic from cmdapi and main

--- cmd/atlas/internal/cmdapi/cmdapi.go
+++ cmd/atlas/internal/cmdapi/cmdapi.go
@@ -11,17 +11,14 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
-	"os"
 	"sort"
 	"strings"
-	"testing"
 	"text/template"
 	"time"
 
 	"ariga.io/atlas/cmd/atlas/internal/cmdext"
 	"ariga.io/atlas/cmd/atlas/internal/cmdlog"
 	cmdmigrate "ariga.io/atlas/cmd/atlas/internal/migrate"
-	"ariga.io/atlas/cmd/atlas/internal/cmdstate"
 	"ariga.io/atlas/cmd/atlas/internal/migratelint"
 	"ariga.io/atlas/schemahcl"
 	"ariga.io/atlas/sql/migrate"
@@ -615,12 +612,6 @@ func stateReader(ctx context.Context, env *Env, config *stateReaderConfig) (*cmd
 	}
 }
 
-const localStateFile = "local-community.json"
-
-// LocalState keeps track of local state to enhance developer experience.
-type LocalState struct {
-	UpgradeSuggested time.Time `json:"v1.us"`
-}
 
 func init() {
 	schemaCmd := schemaCmd()
@@ -706,43 +697,7 @@ type (
 	}
 )
 
-const (
-	envSkipUpgradeSuggestions = "ATLAS_NO_UPGRADE_SUGGESTIONS"
-	oneWeek                   = 7 * 24 * time.Hour
-)
-
-// maySuggestUpgrade informs the user about the limitations of the community edition to stderr
-// at most once a week. The user can disable this message by setting the ATLAS_NO_UPGRADE_SUGGESTIONS
-// environment variable.
-func maySuggestUpgrade(cmd *cobra.Command) {
-	if os.Getenv(envSkipUpgradeSuggestions) != "" || testing.Testing() {
-		return
-	}
-	state := cmdstate.File[LocalState]{Name: localStateFile}
-	prev, err := state.Read()
-	if err != nil {
-		return
-	}
-	if time.Since(prev.UpgradeSuggested) < oneWeek {
-		return
-	}
-	s := `Notice: This Atlas edition lacks support for features such as checkpoints,
-testing, down migrations, and more. Additionally, advanced database objects such as views,
-triggers, and stored procedures are not supported. To read more: https://atlasgo.io/community-edition
-
-To install the non-community version of Atlas, use the following command:
-
-	curl -sSf https://atlasgo.sh | sh
-
-Or, visit the website to see all installation options:
-
-	https://atlasgo.io/docs#installation
-
-`
-	_ = cmdlog.WarnOnce(cmd.ErrOrStderr(), cmdlog.ColorCyan(s))
-	prev.UpgradeSuggested = time.Now()
-	_ = state.Write(prev)
-}
+func maySuggestUpgrade(_ *cobra.Command) {}
 
 // migrateLintSetFlags allows setting extra flags for the 'migrate lint' command.
 func migrateLintSetFlags(*cobra.Command, *migrateLintFlags) {}
